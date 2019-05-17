639bc13148080b2f1ffbf5e297d4db0c913ac64d
jamalmohammed <jamalmohd28051994@gmail.com>
2019-05-17 10:52:05 +0530

    many changes

--- js/controllers/appCtrl.js
+++ js/controllers/appCtrl.js
@@ -1,73 +1,24 @@
-ngApp.controller('myCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+ngApp.controller('myCtrl', ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
     $scope.mainLoaderIs = true;
-
-    $scope.membersList = [{
-        "id": "1",
-        "name": "Jamal Mohammed"
-    }, {
-        "id": "2",
-        "name": "Rajeshree"
-    }, {
-        "id": "3",
-        "name": "Neha"
-    }, {
-        "id": "4",
-        "name": "Jaffar Ali"
-    }, {
-        "id": "5",
-        "name": "Ravi Kore"
-    }, {
-        "id": "6",
-        "name": "Hamza Shaikh"
-    }, {
-        "id": "7",
-        "name": "Shashi Yadav"
-    }, {
-        "id": "8",
-        "name": "Mahesh"
-    }, {
-        "id": "9",
-        "name": "Charan"
-    }, {
-        "id": "10",
-        "name": "Komal"
-    }, {
-        "id": "11",
-        "name": "Yusuf Aslam"
-    }, {
-        "id": "12",
-        "name": "Paras"
-    }, {
-        "id": "13",
-        "name": "Vasuda"
-    }, {
-        "id": "14",
-        "name": "Ameer Shaikh"
-    }, {
-        "id": "15",
-        "name": "Shruti"
-    }, {
-        "id": "16",
-        "name": "Faizan"
-    }, {
-        "id": "17",
-        "name": "Amir"
-    }, {
-        "id": "18",
-        "name": "Abdul Niyaz"
-    }, {
-        "id": "19",
-        "name": "Shiyam Sunder"
-    }, {
-        "id": "20",
-        "name": "Sharuk Khan"
-    }];
-
+    $scope.membersList = [];
     $scope.createdGroupList = [];
     $scope.checkRandomNo = [];
     $scope.membersCount = 5;
     $scope.groupsCount = 4;
-    $scope.membersInGroup = [];
+    //$scope.membersInGroup = [];
+    $scope.membersObj = {
+        "name": "",
+        "empId": "",
+        "department": ""
+    };
+
+    $scope.addMembers = function () {
+        $scope.membersList.push(angular.copy($scope.membersObj));
+        $scope.membersObj.name = "";
+        $scope.membersObj.empId = "";
+        $scope.membersObj.department = "";
+        $('#memberAddModal').modal('hide');
+    };
 
     $scope.randomNoCreator = function () {
         for (var a = $scope.checkRandomNo, i = a.length; i--;) {
@@ -81,10 +32,10 @@ ngApp.controller('myCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
             $scope.checkRandomNo.push(r);
         }
 
-        for (var m = 0; m < $scope.membersCount; m++) {
-            var members = "Member-" + (m + 1);
-            $scope.membersInGroup.push(members);
-        }
+        // for (var m = 0; m < $scope.membersCount; m++) {
+        //     var members = "Member-" + (m + 1);
+        //     $scope.membersInGroup.push(members);
+        // }
 
         for (var g = 0; g < $scope.groupsCount; g++) {
             var group = [];
@@ -106,14 +57,22 @@ ngApp.controller('myCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
     };
 
     $scope.reloadFun = function () {
-        $scope.createdGroupList = [];
-        $scope.checkRandomNo = [];
-        $scope.membersInGroup = [];
-        $scope.autoCreateGroup();
+        var url = "https://api.hosterapp.in/amsnode/getRoleList";
+        $http.get(url).then(function (response) {
+            $scope.students = response.data;
+        });
+
+        $.getJSON("js/json/membersList.json").then(function (result) {
+            console.log('result', result);
+            $scope.membersList = result;
+            $scope.createdGroupList = [];
+            $scope.checkRandomNo = [];
+            $scope.membersInGroup = [];
+            $scope.autoCreateGroup();
+        });
     };
 
     $timeout(function () {
         $scope.mainLoaderIs = false;
     }, 1000);
-    
 }]);
